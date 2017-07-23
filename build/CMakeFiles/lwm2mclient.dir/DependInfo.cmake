244368a5b8eea42d6269afe89a71b8bc4d4ccc89
Vibhor Khurana <vibhorkhurana@gmail.com>
2017-07-23 22:35:34 +0900

    Initial version

---
+++ build/CMakeFiles/lwm2mclient.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/wakaama/core/block1.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/block1.c.o"
+  "/home/pi/wakaama/core/bootstrap.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/bootstrap.c.o"
+  "/home/pi/wakaama/core/data.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/data.c.o"
+  "/home/pi/wakaama/core/discover.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/discover.c.o"
+  "/home/pi/wakaama/core/er-coap-13/er-coap-13.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/er-coap-13/er-coap-13.c.o"
+  "/home/pi/wakaama/core/json.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/json.c.o"
+  "/home/pi/wakaama/core/liblwm2m.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/liblwm2m.c.o"
+  "/home/pi/wakaama/core/list.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/list.c.o"
+  "/home/pi/wakaama/core/management.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/management.c.o"
+  "/home/pi/wakaama/core/objects.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/objects.c.o"
+  "/home/pi/wakaama/core/observe.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/observe.c.o"
+  "/home/pi/wakaama/core/packet.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/packet.c.o"
+  "/home/pi/wakaama/core/registration.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/registration.c.o"
+  "/home/pi/wakaama/core/tlv.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/tlv.c.o"
+  "/home/pi/wakaama/core/transaction.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/transaction.c.o"
+  "/home/pi/wakaama/core/uri.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/uri.c.o"
+  "/home/pi/wakaama/core/utils.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/utils.c.o"
+  "/home/pi/wakaama/shared/commandline.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/commandline.c.o"
+  "/home/pi/wakaama/shared/connection.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/connection.c.o"
+  "/home/pi/wakaama/shared/memtrace.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/memtrace.c.o"
+  "/home/pi/wakaama/shared/platform.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/platform.c.o"
+  "/home/pi/wakaama/client/lwm2mclient.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/lwm2mclient.c.o"
+  "/home/pi/wakaama/client/object_access_control.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_access_control.c.o"
+  "/home/pi/wakaama/client/object_connectivity_moni.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_connectivity_moni.c.o"
+  "/home/pi/wakaama/client/object_connectivity_stat.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_connectivity_stat.c.o"
+  "/home/pi/wakaama/client/object_device.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_device.c.o"
+  "/home/pi/wakaama/client/object_firmware.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_firmware.c.o"
+  "/home/pi/wakaama/client/object_location.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_location.c.o"
+  "/home/pi/wakaama/client/object_security.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_security.c.o"
+  "/home/pi/wakaama/client/object_server.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/object_server.c.o"
+  "/home/pi/wakaama/client/performance_object.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/performance_object.c.o"
+  "/home/pi/wakaama/client/system_api.c" "/home/pi/wakaama/build3/CMakeFiles/lwm2mclient.dir/system_api.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LWM2M_BOOTSTRAP"
+  "LWM2M_CLIENT_MODE"
+  "LWM2M_LITTLE_ENDIAN"
+  "LWM2M_SUPPORT_JSON"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/pi/wakaama/core"
+  "/home/pi/wakaama/shared"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lwm2mclient.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lwm2mclient.dir/lwm2mclient.c.o"
+  "CMakeFiles/lwm2mclient.dir/system_api.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_security.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_server.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_device.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_firmware.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_location.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_connectivity_moni.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_connectivity_stat.c.o"
+  "CMakeFiles/lwm2mclient.dir/object_access_control.c.o"
+  "CMakeFiles/lwm2mclient.dir/performance_object.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/liblwm2m.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/uri.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/utils.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/objects.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/tlv.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/data.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/list.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/packet.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/transaction.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/registration.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/bootstrap.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/management.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/observe.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/json.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/discover.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/block1.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/core/er-coap-13/er-coap-13.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/commandline.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/platform.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/memtrace.c.o"
+  "CMakeFiles/lwm2mclient.dir/home/pi/wakaama/shared/connection.c.o"
+  "lwm2mclient.pdb"
+  "lwm2mclient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lwm2mclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
